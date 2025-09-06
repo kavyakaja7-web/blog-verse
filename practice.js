135b3c35239d7a3aa659e288c0c3c2feff9fb541
kavyakaja <kavyakaja7@gamil.com>
2025-09-06 12:36:52 +0530

    commands

--- practice.js
+++ practice.js
@@ -1,1 +1,36 @@
 console.log("hello world,kavya")
+
+let name ="Dev Astra";
+console.log(name)
+
+let num=18;
+console.log(num)
+
+let islogged=false;
+console.log(islogged)
+
+let x;
+console.log(x)
+console.log(typeof x)
+
+let y=null;
+console.log(y)
+console.log(typeof y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+
+
+let arr =[1,2,3,4,5]
+console.log(arr)
+console.log(typeof arr)
+
+let person = { 
+    name:"kavya",
+    age:18
+}
+
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
